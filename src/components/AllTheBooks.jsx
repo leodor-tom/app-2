d639d5c72f386007c85295077f21751d1636da70
leodor-tom <eleodoritom@gmail.com>
2023-09-05 17:24:09 +0200

    c

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -8,7 +8,12 @@ const AllTheBooks = () => (
       {Books.map((book, index) => (
         <Col key={`book-${index}`} sm="3">
           <Card style={{ height: "500px" }}>
-            <Card.Img variant="top" src={book.img} alt={book.category} style={{ height: "300px" }} />
+            <Card.Img
+              variant="top"
+              src={book.img}
+              alt={book.category}
+              style={{ height: "350px", objectFit: `contain` }}
+            />
             <Card.Body>
               <Card.Title>{book.title}</Card.Title>
               <Card.Text className="mt-3">{book.price}€</Card.Text>
